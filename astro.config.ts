94085d7769555d0615a76c2039c392fc65d71309
thein3rovert <danielolaibi@gmail.com>
2025-05-06 15:29:57 +0100

    com(astro_config_and_types): Understanding the astro configuration and the application types
    -m

--- astro.config.ts
+++ astro.config.ts
@@ -23,100 +23,102 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.url,
-	image: {
-		domains: ["webmention.io"],
-	},
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		icon(),
-		sitemap(),
-		mdx(),
-		robotsTxt(),
-		webmanifest({
-			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
-			name: siteConfig.title,
-			short_name: "Astro_Cactus", // optional
-			description: siteConfig.description,
-			lang: siteConfig.lang,
-			icon: "public/icon.svg", // the source for generating favicon & icons
-			icons: [
-				{
-					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
-					sizes: "180x180",
-					type: "image/png",
-				},
-				{
-					src: "icons/icon-192.png",
-					sizes: "192x192",
-					type: "image/png",
-				},
-				{
-					src: "icons/icon-512.png",
-					sizes: "512x512",
-					type: "image/png",
-				},
-			],
-			start_url: "/",
-			background_color: "#1d1f21",
-			theme_color: "#2bbc8a",
-			display: "standalone",
-			config: {
-				insertFaviconLinks: false,
-				insertThemeColorMeta: false,
-				insertManifestLink: false,
-			},
-		}),
-	],
-	markdown: {
-		rehypePlugins: [
-			rehypeHeadingIds,
-			[rehypeAutolinkHeadings, { behavior: "wrap", properties: { className: ["not-prose"] } }],
-			[
-				rehypeExternalLinks,
-				{
-					rel: ["noreferrer", "noopener"],
-					target: "_blank",
-				},
-			],
-			rehypeUnwrapImages,
-		],
-		remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: [""],
-			},
-		},
-	},
-	// https://docs.astro.build/en/guides/prefetch/
-	prefetch: true,
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-		plugins: [tailwind(), rawFonts([".ttf", ".woff"])],
-	},
-	env: {
-		schema: {
-			WEBMENTION_API_KEY: envField.string({ context: "server", access: "secret", optional: true }),
-			WEBMENTION_URL: envField.string({ context: "client", access: "public", optional: true }),
-			WEBMENTION_PINGBACK: envField.string({ context: "client", access: "public", optional: true }),
-		},
-	},
+  site: siteConfig.url,
+  image: {
+    domains: ["webmention.io"],
+  },
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    icon(),
+    sitemap(),
+    mdx(),
+    robotsTxt(),
+    webmanifest({
+      // This section defines the web app manifest configuration for the site
+      name: siteConfig.title, // The name of the web application
+      short_name: "thein3rovert_blog", // An optional abbreviated name for the web app
+      description: siteConfig.description, // A brief description of the web application
+      lang: siteConfig.lang, // The default language of the web application
+      icon: "public/icon.svg", // The source icon file for generating favicon & icons
+
+      // TODO: Need to understand where its getting the icons from
+      icons: [
+        {
+          src: "icons/apple-touch-icon.png", // Icon used in src/components/BaseHead.astro L:26
+          sizes: "180x180", // Icon dimensions
+          type: "image/png", // MIME type of the icon
+        },
+        {
+          src: "icons/icon-192.png", // Icon file path for 192x192 size
+          sizes: "192x192", // Icon dimensions
+          type: "image/png", // MIME type of the icon
+        },
+        {
+          src: "icons/icon-512.png", // Icon file path for 512x512 size
+          sizes: "512x512", // Icon dimensions
+          type: "image/png", // MIME type of the icon
+        },
+      ],
+      start_url: "/", // The URL that loads when the web app is launched
+      background_color: "#1d1f21", // Background color used on the launch screen
+      theme_color: "#2bbc8a", // Default theme color of the application
+      display: "standalone", // Display mode of the web application
+      config: {
+        insertFaviconLinks: false, // Configuration to not insert favicon links
+        insertThemeColorMeta: false, // Configuration to not insert meta theme color
+        insertManifestLink: false, // Configuration to not insert manifest link
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, { behavior: "wrap", properties: { className: ["not-prose"] } }],
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["noreferrer", "noopener"],
+          target: "_blank",
+        },
+      ],
+      rehypeUnwrapImages,
+    ],
+    remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""],
+      },
+    },
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+    plugins: [tailwind(), rawFonts([".ttf", ".woff"])],
+  },
+  env: {
+    schema: {
+      WEBMENTION_API_KEY: envField.string({ context: "server", access: "secret", optional: true }),
+      WEBMENTION_URL: envField.string({ context: "client", access: "public", optional: true }),
+      WEBMENTION_PINGBACK: envField.string({ context: "client", access: "public", optional: true }),
+    },
+  },
 });
 
 function rawFonts(ext: string[]) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// @ts-expect-error:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // @ts-expect-error:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,83 +1,88 @@
+/**
+  * The SiteConfig interface defines the configuration structure for a website.
+  * It includes information such as the author's name, date formatting options,
+  * site description, primary language, Open Graph locale, site title, and base URL.
+  */
 export interface SiteConfig {
-	author: string;
-	date: {
-		locale: string | string[] | undefined;
-		options: Intl.DateTimeFormatOptions;
-	};
-	description: string;
-	lang: string;
-	ogLocale: string;
-	title: string;
-	url: string;
+  author: string;
+  date: {
+    locale: string | string[] | undefined;
+    options: Intl.DateTimeFormatOptions;
+  };
+  description: string;
+  lang: string;
+  ogLocale: string;
+  title: string;
+  url: string;
 }
 
 export interface PaginationLink {
-	srLabel?: string;
-	text?: string;
-	url: string;
+  srLabel?: string;
+  text?: string;
+  url: string;
 }
 
 export interface SiteMeta {
-	articleDate?: string | undefined;
-	description?: string;
-	ogImage?: string | undefined;
-	title: string;
+  articleDate?: string | undefined;
+  description?: string;
+  ogImage?: string | undefined;
+  title: string;
 }
 
 /** Webmentions */
 export interface WebmentionsFeed {
-	children: WebmentionsChildren[];
-	name: string;
-	type: string;
+  children: WebmentionsChildren[];
+  name: string;
+  type: string;
 }
 
 export interface WebmentionsCache {
-	children: WebmentionsChildren[];
-	lastFetched: null | string;
+  children: WebmentionsChildren[];
+  lastFetched: null | string;
 }
 
 export interface WebmentionsChildren {
-	author: Author | null;
-	content?: Content | null;
-	"mention-of": string;
-	name?: null | string;
-	photo?: null | string[];
-	published?: null | string;
-	rels?: Rels | null;
-	summary?: Summary | null;
-	syndication?: null | string[];
-	type: string;
-	url: string;
-	"wm-id": number;
-	"wm-private": boolean;
-	"wm-property": string;
-	"wm-protocol": string;
-	"wm-received": string;
-	"wm-source": string;
-	"wm-target": string;
+  author: Author | null;
+  content?: Content | null;
+  "mention-of": string;
+  name?: null | string;
+  photo?: null | string[];
+  published?: null | string;
+  rels?: Rels | null;
+  summary?: Summary | null;
+  syndication?: null | string[];
+  type: string;
+  url: string;
+  "wm-id": number;
+  "wm-private": boolean;
+  "wm-property": string;
+  "wm-protocol": string;
+  "wm-received": string;
+  "wm-source": string;
+  "wm-target": string;
 }
 
 export interface Author {
-	name: string;
-	photo: string;
-	type: string;
-	url: string;
+  name: string;
+  photo: string;
+  type: string;
+  url: string;
 }
 
 export interface Content {
-	"content-type": string;
-	html: string;
-	text: string;
-	value: string;
+  "content-type": string;
+  html: string;
+  text: string;
+  value: string;
 }
 
 export interface Rels {
-	canonical: string;
+  canonical: string;
 }
 
 export interface Summary {
-	"content-type": string;
-	value: string;
+  "content-type": string;
+  value: string;
 }
 
 export type AdmonitionType = "tip" | "note" | "important" | "caution" | "warning";
