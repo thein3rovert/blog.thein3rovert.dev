979fc7959254487b1c380ec544a942b267ac50af
thein3rovert <danielolaibi@gmail.com>
2025-05-06 16:51:15 +0100

    feat(social-icon): Add new social icons

---
+++ src/data/constants.ts
@@ -0,0 +1,43 @@
+export const MENU_LINKS = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/posts",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Videos",
+    path: "/videos",
+  },
+  {
+    title: "Photos",
+    path: "https://photos.stevedylan.dev",
+  },
+];
+
+// ! Remember to add your own socials
+export const SOCIAL_LINKS = {
+  github: "https://github.com/thein3rovert",
+  // twitter: "https://twitter.com/stevedylandev",
+  // youtube: "https://youtube.com/@stevedylandev",
+  // telegram: "https://telegram.me/stevedylandev",
+  // ethereum: "https://rainbow.me/stevedylandev.eth",
+  // farcaster: "https://warpcast.com/stevedylandev.eth",
+  // medium: "https://medium.com/@stevedylandev",
+  linkedin: "https://www.linkedin.com/in/samad-olaibi/",
+  // photos: "https://photos.stevedylan.dev",
+  threads: "https://threads.net/thein3rovert",
+  website: "/",
+  // coffee: "https://buymeacoffee.com/stevedylandev",
+  // orbiter: "https://orbiter.host"
+};
