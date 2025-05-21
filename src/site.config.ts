d697e36ad01f01178dc4e2ad157b86f1bc5c5155
thein3rovert <danielolaibi@gmail.com>
2025-05-21 18:31:29 +0100

    feat(code-page): Created a new code page

--- src/site.config.ts
+++ src/site.config.ts
@@ -48,6 +48,10 @@ export const menuLinks: { path: string; title: string }[] = [
     path: "/notes/",
     title: "Notes",
   },
+  {
+    path: "/code/",
+    title: "Code"
+  },
 ];
 
 // https://expressive-code.com/reference/configuration/
